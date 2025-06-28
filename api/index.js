0de9126d19561474cc1408e12037e71a7a495eea
Youngson335 <mstolstik@gmail.com>
2025-06-28 20:54:15 +0900

    fix content

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.status(200).json({ message: "Hello from Vercel!" });
+  res.status(200).json({ message: "tema занимаесят не понять чем пока что " });
 }
